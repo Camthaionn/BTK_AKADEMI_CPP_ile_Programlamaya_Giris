17ac9efeade10468cf13c8430c2e8647fb5efe08
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-11-02 21:05:22 +0300

    Add files via upload

---
+++ 52.Preprocessor2.cpp
@@ -0,0 +1,12 @@
+﻿
+#include <iostream>
+
+//compiler compiled all files so it saw the method
+int sum(int a, int b);
+
+int main()
+{
+    //This function comes from another cpp file
+    std::cout << sum(10, 10);
+}
+
