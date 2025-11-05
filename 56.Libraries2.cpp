765ab2044b55bc26477e1409ceb953f2a1d31dba
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-11-05 18:34:20 +0300

    Add files via upload

---
+++ 56.Libraries2.cpp
@@ -0,0 +1,30 @@
+﻿
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+int main()
+{
+    //The address of text
+    string* write = new string;
+
+    //Creates the file
+    ofstream* folder = new ofstream("folder.txt");
+
+    //Checks if the folder is exists
+    if (folder->is_open()) {
+
+        //Gets the input
+        cin >> *write;
+
+        //Text has been assigned
+        *folder << *write;
+
+        //Closed the folder
+        folder->close();
+    }
+    
+}
