520ad3dfded68abf56b8060704d74ec9de7a11aa
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-11-05 18:20:01 +0300

    Add files via upload

---
+++ 55.Lİbraries.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+    string test = " Hello string";
+
+    //This adds string into the test variable
+    test.insert(7, "dear ");
+    
+    //This adds string value end of the variable
+    test.append(" bye");
+
+
+    cout << test;
+}
